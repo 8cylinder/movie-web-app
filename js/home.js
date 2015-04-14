42155b50c5eab74aa5e6c2255b36f5b2c4be2d6f
Sheldon McGrandle <developer@rednemesis.com>
2015-04-14 09:59:15 -0400

    Rewrite the dropdown code
    The old was complicated and confusing, this simplifies it.

--- js/home.js
+++ js/home.js
@@ -62,63 +62,55 @@ var home = (function(pub, $, hb, c, movies, config, movielists)
       $('body').pagecontainer('change', '#details');
     });
 
-    // set a global var because I'm lazy right now.
-    window.last_taphold_id = '';
+
     $('.itemcontainer').on('taphold', function(){
-      var movie_id = this.id;
-      // hide lists if this is a second taphold on same row
-      if(window.last_taphold_id == movie_id){
-        $('.home_lists').empty();
-        c.log('remove drop down')
-        window.last_taphold_id = '';
-        return false;
+
+      // CLOSE DROPDOWN
+      if($('.home_lists', this).children().length > 0){
+        $('.home_lists').empty(); //close all dropdowns
+
+      // OPEN DROPDOWN
       } else {
-        window.last_taphold_id = movie_id;
-      }
-      // get the lists and check if this movie
-      // is in any of them
-      var all_lists = movielists.get_all_but_0();
-      for (var i=0; i<all_lists.length; i++){
-        if (all_lists[i].movies.contains(movie_id)){
-          all_lists[i].cssstate = 'on';
-          all_lists[i].state = '1';
-        } else {
-          all_lists[i].cssstate = 'off';
-          all_lists[i].state = '0';
+        $('.home_lists').empty(); //close all dropdowns
+        var movie_id = $(this).attr('id');
+        // get the lists and check if this movie
+        // is in any of them
+        var all_lists = movielists.get_all_but_0();
+        for (var i=0; i<all_lists.length; i++){
+          if (all_lists[i].movies.contains(movie_id)){
+            all_lists[i].cssstate = 'on';
+            all_lists[i].state = '1';
+          } else {
+            all_lists[i].cssstate = 'off';
+            all_lists[i].state = '0';
+          }
         }
+        // build the template
+        var template_source = $('#home_list_template').html();
+        var template = hb.compile(template_source);
+        var html = template({'lists':all_lists});
+
+        // insert the dropdown
+        var insert_point = '#__ .home_lists'.merge([movie_id]);
+        $(insert_point).html(html);
+
+        // add the events for the list buttons
+        $('.home_list_item').on('tap', null, {'movie_id':movie_id}, function(event){
+          var state = $(this).data('state');
+          if (state == 0){
+            movielists.add_movie(movie_id, event.data.movie_id);
+          } else if (state == 1) {
+            movielists.remove_movie(movie_id, event.data.movie_id);
+          }
+          $('.home_lists').empty();
+          return false;
+        });
+        // delete movie
+        $('.home_delete').on('tap', null, {movie_id:movie_id}, function(event){
+          movies.delete_single(event.data.movie_id);
+          pub.show_movies();
+        });
       }
-
-      // build the template
-      var template_source = $('#home_list_template').html();
-      var template = hb.compile(template_source);
-      var html = template({'lists':all_lists});
-
-      // hide others and display the list
-      //$('.home_lists').empty();
-      var insert_point = '#__ .home_lists'.merge([this.id]);
-      $(insert_point).html(html);
-      $('.home_drop_down').slideDown('slow', function(){
-        c.log('animate done', this)
-      });
-
-      $('.home_list_item').on('tap', null, {'movie_id':movie_id}, function(event){
-        var state = $(this).data('state');
-        if (state == 0){
-          movielists.add_movie(this.id, event.data.movie_id);
-        } else if (state == 1) {
-          movielists.remove_movie(this.id, event.data.movie_id);
-        }
-        $('.home_lists').empty();
-        return false;
-      });
-
-      // delete movie
-      $('.home_delete').on('tap', null, {movie_id:movie_id}, function(event){
-        movies.delete_single(event.data.movie_id);
-        pub.show_movies();
-      });
-
-      return true;
     });
   };
   return pub;
